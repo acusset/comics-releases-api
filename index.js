f8b89ffd4d4249b60fe4d74e1df15c1472400f25
Antoine Cusset <acusset@clever-age.com>
2017-03-10 13:00:16 +0100

    wait pipe file before parse

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ var moment = require('moment');
 var slug, date;
 var filePath = path.join(__dirname, 'newreleases.txt');
 var jsonPath = path.join(__dirname, 'newreleases.json');
+var menuPath = path.join(__dirname, 'menu.json');
 var index = path.join(__dirname, '/public/index.html');
+
 var dateRegex = /(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}/g;
 var regex = /(^[A-Z]{3}[0-9]{6})\s(.+)\s\$(([0-9]+\.[0-9]{2})|PI)/i;
 
@@ -21,12 +23,12 @@ app.get('/api', function (req, res) {
     res.send(fs.readFileSync(jsonPath, {encoding: 'utf-8', flag: 'r'}));
 });
 
-app.get('/', function (req, res) {
-    var html = fs.readFileSync(index, 'utf8');
-    res.render(html);
+app.get('/api/menu', function (req, res) {
+    res.setHeader('Content-Type', 'application/json;charset=utf-8');
+    res.send(fs.readFileSync(menuPath, {encoding: 'utf-8', flag: 'r'}));
 });
 
-schedule.scheduleJob('*/30 * * * * *', function () {
+schedule.scheduleJob('42 3 * * * *', function () {
     getFile();
 });
 
@@ -34,14 +36,19 @@ function getFile() {
     var target = 'https://www.previewsworld.com/shipping/newreleases.txt';
     var file = fs.createWriteStream(filePath);
     https.get(target, function (response) {
-        response.statusCode === 200 ? response.pipe(file) : false;
-        parseFile();
+        if (response.statusCode === 200) {
+            response.pipe(file);
+            file.on('finish', function () {
+                parseFile();
+            });
+        }
     });
 }
 
 function parseFile() {
     fs.readFile(filePath, 'utf-8', function (err, data) {
         var tmp = {};
+        var types = [];
         if (err) throw err;
         var lines = data.split('\n');
         for (var i = 0; i < lines.length; i++) {
@@ -66,12 +73,19 @@ function parseFile() {
             } else if (i > 7) { // skip first lines
                 var title = line.trim();
                 slug = slugify(title);
+                var type = {
+                    name: title,
+                    slug: slug
+                };
+                types.push(type);
                 tmp[slug] = [];
             }
         }
         tmp = JSON.stringify(tmp);
-        console.log(tmp);
+        types = JSON.stringify(types);
+        console.log(types);
         fs.writeFileSync(jsonPath, tmp, 'utf8');
+        fs.writeFileSync(menuPath, types, 'utf8');
     });
 }
 
@@ -79,10 +93,12 @@ function slugify(text) {
     return text.toString().toLowerCase()
         .replace(/\s+/g, '-')           // Replace spaces with -
         .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
+        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
         .replace(/^-+/, '')             // Trim - from start of text
         .replace(/-+$/, '');            // Trim - from end of text
 }
 
 app.listen(3000, function () {
     console.log('Listening on port 3000!');
+    getFile();
 });
